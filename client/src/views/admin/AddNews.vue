797abbdccf2bedb8c947fbe7eaf6874fe18388b3
YuraA30 <61061567+YuraA30@users.noreply.github.com>
2021-03-16 23:39:48 +0200

    Editor

--- client/src/views/admin/AddNews.vue
+++ client/src/views/admin/AddNews.vue
@@ -6,14 +6,21 @@
       </div>
       <div class="text-left mt-4">
         <label>Заголовок новини: </label>
-        <input type="text" class="ml-4 w-50 w-md-100 form-control" maxlength="50" required />
+        <input
+          type="text"
+          class="ml-4 w-50 w-md-100 form-control"
+          maxlength="50"
+          required
+        />
         <label class="mt-3">Обкладинка: </label>
         <button class="ml-4 button" @click="upload()">Завантажити</button>
         <label class="mt-3">Текст новини: </label>
       </div>
-      <div class="news-text">Тут редактор</div>
+      <vue-editor v-model="content"></vue-editor>
       <div class="mt-5">
-        <button class="my-2 mx-2 d-inline button gray">Попередній перегляд</button>
+        <button class="my-2 mx-2 d-inline button gray">
+          Попередній перегляд
+        </button>
         <button class="my-2 mx-2 d-inline button">Створити</button>
       </div>
     </form>
@@ -21,14 +28,23 @@
 </template>
 
 <script>
+import { VueEditor } from "vue2-editor";
 export default {
-    methods:{
-        upload()
-        {
-            alert("Воно не робе")
-        }
-    }
-}
+  components: {
+    VueEditor,
+  },
+  methods: {
+    upload() {
+      alert("Воно не робе");
+    },
+  },
+  data() {
+    return {
+      content:
+        "<h1>Some initial content</h1> <i><b>лдакдоадойдйрйакцлайкц</b></i>",
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -37,11 +53,11 @@ export default {
   background-color: rgb(245, 245, 245);
 }
 
-.gray{
-    background-color: rgb(117, 117, 117);
+.gray {
+  background-color: rgb(117, 117, 117);
 }
 
-.button{
-    width: 220px;
+.button {
+  width: 220px;
 }
 </style>
