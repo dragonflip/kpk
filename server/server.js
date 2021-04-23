620d61d8a17baf27e6c79bd79ec6def929281bcc
sviatikSta <apelsinovych@gmail.com>
2021-04-23 20:11:44 +0300

    Added the ability to insert a 50MB photo

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 
 // Middlewares
 app.use(express.static(__dirname + "/public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ limit: '50mb',extended: true, parameterLimit:50000 }));
 
 app.use("/api/add-news", require("./routes/news"));
 app.use("/api/admin-department", require("./routes/department"));
